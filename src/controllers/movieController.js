1ac37d329472c08a1f67396cf38c4f907148b05c
Cleyton Eduardo Rezende <cleytonmgrezendedev@gmail.com>
2021-12-29 17:24:05 -0300

    feature: ADD Controllers Functions

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,4 +1,4 @@
-import { readMovies } from "../services/jsonServices";
+import { createMovie, deleteMovie, readMovies, updateMovie } from "../services/jsonServices";
 
 export default (app) => {
 
@@ -13,7 +13,43 @@ export default (app) => {
 
             response.json(data)
         })
+
+        app.post('/movie', async (request, response) => {
+            const body = request.body
+
+            const idCreateMovie = await createMovie(body)
+
+            response.status(201).json({
+                response: idCreateMovie
+            })
+        })
+
+        app.put('/movie/:id', async (request, response) => {
+            const body = request.body;
+            const id = request.params.id;
+            body.id = Number(id)
+            console.log("BODY: ", body, id)
+
+            const idUpdateMovie = await updateMovie(body);
+
+            response.status(201).json({
+                response: idUpdateMovie
+            })
+        })
+
+        app.delete('/movie/:id', async (request, response) => {
+            const id = Number(request.params.id);
+            console.log(id)
+
+            const idDeleteMovie = await deleteMovie(id);
+
+            response.status(201).json(
+                {
+                    response: idDeleteMovie
+                }
+            )
+        })
+
     }
 
 }
-
